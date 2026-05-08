415320cf594d2eb20a430e6f14a11c18d81d8485
jegcu26 <jegor.cupalov@gmail.com>
2026-05-08 11:57:30 +0200

    jegor_2

--- Pynq/jegor.c
+++ Pynq/jegor.c
@@ -36,19 +36,26 @@ void spin(void){
     wait_until_done();
 
 }
+void 180(void){
+
+    stepper_set_speed(25000, 25000);
+    stepper_steps(1250, -1250);
+    wait_until_done();
+
+}
 
 void look_around(void){
 
     stepper_set_speed(30000, 30000);
-    stepper_steps(312, -312);
+    stepper_steps(400, -400);
     wait_until_done();
 
     stepper_set_speed(30000, 30000);
-    stepper_steps(-625, 625);
+    stepper_steps(-800, 800);
     wait_until_done();
 
     stepper_set_speed(30000, 30000);
-    stepper_steps(312, -312);
+    stepper_steps(400, -400);
     wait_until_done();
 
 }
@@ -58,12 +65,45 @@ void forward(int speed, int steps){
     stepper_set_speed(speed, speed);
     stepper_steps(steps, steps);
 
-    
-
     wait_until_done();
 }
 
-void obstacle
+void obstacle(void){
+
+    turn_left();
+    look_around();
+
+    if(distance >> 30){
+        while(1){
+            forward(15000, 400);
+            turn_right();
+
+            if(distance >> 30){
+                return;
+            }
+            turn_left();
+        }
+    }
+
+    if(distance << 30){
+        180();
+        if(distance >> 30){
+            while(1){
+                forward(15000, 400);
+                turn_left();
+
+                if(distance >> 30){
+                    return;
+                }
+                turn_right();
+            }
+        }
+        if(distance << 30){
+            turn_right();
+            return;
+        }
+    }
+}
 
 
 int main(void) {
