fd3cfccf88e337d45c09a5cf99b2ef7e89a575a4
Marco de Vries Aguirre <m.d.vries.aguirre@student.tue.nl>
2026-05-08 11:12:57 +0200

    position functio

--- Pynq/algorithm.c
+++ Pynq/algorithm.c
@@ -1,3 +1,35 @@
 #include "shared.h"
 #include <stdio.h>
 #include <unistd.h>
+
+typedef struct{
+int dir;
+int x;
+int y;
+pthread_mutex_t lock;
+} robpos;
+
+void posup(robpos *state, int distance){
+    pthread_mutex_lock(&state->lock);
+    switch(state->dir){
+        case 1: state->y += distance; break; //north 
+        case 2: state->x += distance; break; //east
+        case 3: state->y -= distance; break; //south
+        case 4: state->x -= distance; break; //west
+    }
+    pthread_mutex_unlock(&state->lock);
+}
+
+void dirup(robpos *state, int turn){
+    pthread_mutex_lock(&state->lock);
+    //turn=1 for right
+    //turn=-1 for left
+    if (state->dir==4 && turn==1){
+        state->dir = 1;
+    } else if (state->dir==1 && turn==-1){
+        state->dir = 4;
+    } else {
+        state->dir = (state->dir + turn);
+    }
+    pthread_mutex_unlock(&state->lock);
+}
