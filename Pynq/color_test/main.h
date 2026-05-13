5c883a45e67c1e20ae5ced08934c4eb35f77491c
TheCreator670 <petrucalingherman2006@gmail.com>
2026-05-13 16:57:39 +0200

    Update main.h

--- Pynq/color_test/main.h
+++ Pynq/color_test/main.h
@@ -1,1 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdint.h>
+#include "shared.h"
+
+void color_sensor_init(void);
+color_result_t color_sensor_read(void);
+const char *color_to_string(color_t color);
+uint8_t color_to_code(color_t color);
+void *color_sensor_thread(void *arg);
+
+#endif
