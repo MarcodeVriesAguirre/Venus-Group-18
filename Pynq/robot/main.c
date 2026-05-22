fd4e47f5a4bbff476a5c0f998370a609121d6cc3
Marco de Vries Aguirre <m.d.vries.aguirre@student.tue.nl>
2026-05-22 10:08:31 +0200

    filling in a few function

--- Pynq/robot/main.c
+++ Pynq/robot/main.c
@@ -4,6 +4,9 @@
 #include <math.h>
 #include "shared.h"
 
+#define gridSize 150
+#define blockSize 3
+
 extern void* distanceR(void *arg); // Tells he compiler that the files are elsewhere for these functions.
 extern void* colorR(void *arg);
 extern void* temperatureR(void *arg);
@@ -13,6 +16,7 @@ extern void* actuationR(void *arg);
 robpos global_state = {0, 0, 1, PTHREAD_MUTEX_INITIALIZER};
 
 void posup(int distance){ //function for updating x and y coordinates
+
 }
 
 void dirup(int turn){ //function for updating the direction
@@ -42,10 +46,18 @@ void walkaound()
 
 void createMap(void)
 {
+    //Assumptions: The map is going to be 1.5x1.5 meters, each grid block will be 3cmx3cm.
+    // the created map needs to be double the size of the theoretical map
+    int grid[gridSize/blockSize][gridSize/blockSize]={0};
 }
 
 int coordTranslate(pos)
 {
+    //The grid is represented as a matrix with dimensions [gridSize/blockSize]x[gridSize/blockSize]
+    //and the 0, 0 isn't going to be 0, 0 on the matrix. coordinates therefore need to be translated.
+    int realPos; //this is the position translated to the matrix.
+    realPos=round(round(pos)/blockSize)+(gridSize/blockSize);
+    return realPos;
 }
 
 void sendmap(xcell, ycell) 
