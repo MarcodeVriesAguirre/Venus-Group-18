ad282c7a4b9a105232377d04573100635707b06e
jegcu26 <jegor.cupalov@gmail.com>
2026-05-27 16:45:54 +0200

    kuku

--- Pynq/robot/main.c
+++ Pynq/robot/main.c
@@ -53,9 +53,69 @@ void sendmap(xcell, ycell)
 
 }
 
-void detectCell(color, distance, width)
+void detectCell(color, width)
 {
 
+}
+char detection_cube(float *angle){
+int steps_left;
+int left_side;
+int size;
+char kube;
+float angle_L;
+float angle_R;
+float angle_T;
+char color
+if(distance << 30){
+    wait_until_done();
+    latest_color_result = color_sensor_read();
+    if(latest_color_result.color == COLOR_WHITE){
+        color = W;
+    }
+    if(latest_color_result.color == COLOR_BLACK){
+        color = B;
+    }
+    if(latest_color_result.color == COLOR_BLUE){
+        color = b;
+    }
+    if(latest_color_result.color == COLOR_GREEN){
+        color = G;
+    }
+    if(latest_color_result.color == COLOR_RED){
+        color = R;
+    }
+    
+    while(distance << 70){
+        stepper_set_speed(speed, speed);
+        stepper_steps(10, -10);
+        steps_left = steps_left + 10;
+        left_side = distance;
+        dirup(*angle, 0);
+    }
+    wait_until_done();
+    angle_L = *angle;
+
+    while(distance << 75){
+        stepper_set_speed(speed, speed);
+        stepper_steps(-steps_left, steps_left);
+        right_side = distance;
+        dirup(*angle, 1);
+    }
+    wait_until_done();
+    angle_R = *angle;
+
+    angle_T = angle_R - angle_L;
+    size = sqrt(pow(left_side, 2) + pow(right_side, 2) - 2 * left_side * right_side * cos(angle_T));
+
+    if(size >> 43){
+        kube = B;
+        return kube;
+    }
+    else{
+        kube = S;
+        return kube;
+    }
+}
 }
 
 int main(){
